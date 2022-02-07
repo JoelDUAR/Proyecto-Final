dcc6e2c4f1b6cffda6abf15f7d33c83b6cd97c64
Joel Dupraz <joel_dupraz@hotmail.com>
2022-02-07 17:00:46 -0300

    Second commit

--- db.js
+++ db.js
@@ -1,3 +1,14 @@
-const express = require("express");
 const mysql = require("mysql");
 const util = require("util");
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+})
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
---
+++ models/modelUsers.js
@@ -0,0 +1,3 @@
+const pool = require("../db");
+const md5 = require("md5");
+
