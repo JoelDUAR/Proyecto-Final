8ffc999070c0b397dc99a4194198be81633ab9c0
Joel Dupraz <joel_dupraz@hotmail.com>
2022-02-09 20:08:54 -0300

    Eighth commit

--- routes/Contact.js
+++ routes/Contact.js
@@ -3,14 +3,29 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const {body, validationResult} = require("express-validator");
 
 router.get("/", (req, res) =>{
     res.render("Contact");
 });
 
+const validationForm = [
+    body("name", "Tenes que ingresar tu nombre").exists().isLength({min:2}),
+    body("lastname", "Tenes que ingresar tu apellido").exists().isLength({min:2}),
+    body("user", "Tenes que ingresar tu usuario").exists().isLength({min:2}),
+    body("mail", "Tenes que ingresar tu email").exists().isEmail(),
+    body("message", "Su mensaje debe contener entre 10 y 300 caracteres").exists().isLength({min: 10, max: 300}),
 
-router.post("/", (req,res) =>{
-    console.log(req.body)
+];
+
+router.post("/", validationForm, async (req,res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const dataForm = req.body;
+        const arrErrors = errors.array();
+        res.render("Contact", {dataForm, arrErrors});
+
+    }
     const emailMsg = {
         to: "atencionyireh@ConstantSourceNode.com",
         from: req.body.email,
@@ -19,6 +34,8 @@ router.post("/", (req,res) =>{
         Contactar al mail: ${req.body.mail}`
     };
 
+
+    
     const transport = nodemailer.createTransport({
         host: process.env.MT_HOST,
         port: process.env.MT_PORT,
@@ -27,11 +44,19 @@ router.post("/", (req,res) =>{
           pass: process.env.MT_PASS
         }
       });
+if(errors.isEmpty()){
 
+let sendMessageStatus = await transport.sendMail(emailMsg);
+let messageStatus = "";
 
-transport.sendMail(emailMsg);
+if(sendMessageStatus.rejected.length){
+messageStatus ="No se pudo enviar tu mensaje"
+} else{
+    messageStatus = "Mensaje enviado"
+}
 res.render("Contact", {
-    message: "Mensaje Enviado"
+    messageStatus,
 });
+}
 });
 module.exports = router;
